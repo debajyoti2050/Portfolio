98185d8d3f4595011f13f67b0579c8483f19773f
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-03-15 18:34:42 +0530

    new

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Debajyoti</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h1>Debajyoti Bhattacharjee</h1>
+        <h5 className="text-light">Fullstack Web Developer</h5>
         <CTA />
 
         <HeaderSocials />
